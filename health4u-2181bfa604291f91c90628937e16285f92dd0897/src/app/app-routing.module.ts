346cb0766979ff98abc10bc451f28876e8b8e361
Sittinon_2501 <sittinon.to@ku.th>
2024-09-28 22:58:31 +0700

    100%Complete

--- health4u-2181bfa604291f91c90628937e16285f92dd0897/src/app/app-routing.module.ts
+++ health4u-2181bfa604291f91c90628937e16285f92dd0897/src/app/app-routing.module.ts
@@ -17,23 +17,44 @@ import { CartComponent } from './user/cart/cart.component';
 import { UserOrderComponent } from './user/user-order/user-order.component';
 import { AuthGuard } from './guards/auth.guard';
 import { OrderHistoryComponent } from './user/order-history/order-history.component';
+import { AdminReportComponent } from './admin/admin-report/admin-report.component';
 
 // Define all routes here
 const routes: Routes = [
   { path: '', redirectTo: '/categories', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'products', component: ProductListComponent,canActivate: [AuthGuard] },
-  { path: 'products/:id', component: ProductDetailComponent,canActivate: [AuthGuard] },
+  {
+    path: 'products',
+    component: ProductListComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
+    canActivate: [AuthGuard],
+  },
 
-  { path: 'profile', component: ProfileComponent,canActivate: [AuthGuard] },
-  { path: 'categories', component: CategoryComponent ,canActivate: [AuthGuard]},
-  { path: 'products/category/:category', component: ProductListComponent ,canActivate: [AuthGuard]}, // ใช้สำหรับกรณีที่มี URL แบบนี้
-  { path: 'footer', component: FooterComponent,canActivate: [AuthGuard] },
-  { path: 'blogs', component: BlogsComponent,canActivate: [AuthGuard] },
-  { path: 'cart', component: CartComponent,canActivate: [AuthGuard] },
-  { path: 'orders', component: UserOrderComponent,canActivate: [AuthGuard] },
-  {path : 'order-history', component: OrderHistoryComponent,canActivate: [AuthGuard]},
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  {
+    path: 'categories',
+    component: CategoryComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'products/category/:category',
+    component: ProductListComponent,
+    canActivate: [AuthGuard],
+  }, // ใช้สำหรับกรณีที่มี URL แบบนี้
+  { path: 'footer', component: FooterComponent, canActivate: [AuthGuard] },
+  { path: 'blogs', component: BlogsComponent, canActivate: [AuthGuard] },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
+  { path: 'orders', component: UserOrderComponent, canActivate: [AuthGuard] },
+  {
+    path: 'order-history',
+    component: OrderHistoryComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'admin/dashboard',
     component: AdminDashboardComponent,
@@ -54,8 +75,12 @@ const routes: Routes = [
     component: AdminCategoryComponent,
     canActivate: [AdminGuard],
   },
+  {
+    path: 'admin/reports',
+    component: AdminReportComponent,
+    canActivate: [AdminGuard],
+  },
   { path: '**', redirectTo: '/categories' },
-
 ];
 
 @NgModule({
